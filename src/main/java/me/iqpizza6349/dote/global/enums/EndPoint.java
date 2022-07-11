6f69442d4e87b890b7f2b7fbfc38cd9f0422da15
iqpizza6349 <iqpizza6349@gmail.com>
2022-07-11 12:30:04 +0900

    :wrench: RestTemplate 설정

---
+++ src/main/java/me/iqpizza6349/dote/global/enums/EndPoint.java
@@ -0,0 +1,15 @@
+package me.iqpizza6349.dote.global.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum EndPoint {
+    AUTH("http://dauth.b1nd.com/api"),
+    OPEN_API("http://open.dodam.b1nd.com/api");
+
+    private final String endPoint;
+
+    EndPoint(String endPoint) {
+        this.endPoint = endPoint;
+    }
+}
