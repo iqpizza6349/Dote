632c1ca9904f9152c1e5bcbd6866dadda35a477d
iqpizza6349 <iqpizza6349@gmail.com>
2022-07-13 21:56:25 +0900

    :hammer: 회원 투표 엔티티 예외 클래스 추가

--- src/main/java/me/iqpizza6349/dote/domain/team/entity/MemberTeam.java
+++ src/main/java/me/iqpizza6349/dote/domain/team/entity/MemberTeam.java
@@ -1,12 +1,17 @@
 package me.iqpizza6349.dote.domain.team.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import me.iqpizza6349.dote.domain.member.entity.Member;
 import me.iqpizza6349.dote.domain.team.entity.embed.MemberTeamId;
+import me.iqpizza6349.dote.global.exception.BusinessException;
+import org.springframework.http.HttpStatus;
 
 import javax.persistence.*;
 
 @Getter
+@AllArgsConstructor @NoArgsConstructor
 @Entity
 @IdClass(MemberTeamId.class)
 public class MemberTeam {
@@ -21,4 +26,9 @@ public class MemberTeam {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    public static class AlreadyVotedException extends BusinessException {
+        public AlreadyVotedException() {
+            super(HttpStatus.CONFLICT, "이미 투표를 진행하였습니다.");
+        }
+    }
 }
--- src/main/java/me/iqpizza6349/dote/domain/team/entity/embed/MemberTeamId.java
+++ src/main/java/me/iqpizza6349/dote/domain/team/entity/embed/MemberTeamId.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @AllArgsConstructor @NoArgsConstructor
 public class MemberTeamId implements Serializable {
@@ -11,4 +12,16 @@ public class MemberTeamId implements Serializable {
     private int member;
     private long team;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MemberTeamId that = (MemberTeamId) o;
+        return member == that.member && team == that.team;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(member, team);
+    }
 }
