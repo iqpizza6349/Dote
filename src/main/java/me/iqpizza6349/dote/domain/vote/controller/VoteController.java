0d9fe8ed6e464f9abaefd198eb09c871a808a77e
iqpizza6349 <iqpizza6349@gmail.com>
2022-07-15 11:08:55 +0900

    :sparkles: 투표 컨트롤러 항목 조회 추가

--- src/main/java/me/iqpizza6349/dote/domain/vote/controller/VoteController.java
+++ src/main/java/me/iqpizza6349/dote/domain/vote/controller/VoteController.java
@@ -2,6 +2,7 @@ package me.iqpizza6349.dote.domain.vote.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.iqpizza6349.dote.domain.member.entity.Member;
+import me.iqpizza6349.dote.domain.team.ro.TeamResponseDto;
 import me.iqpizza6349.dote.domain.vote.dto.BallotDto;
 import me.iqpizza6349.dote.domain.vote.dto.VoteDto;
 import me.iqpizza6349.dote.domain.vote.ro.BallotRO;
@@ -14,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/vote")
@@ -25,20 +27,28 @@ public class VoteController {
     @AuthToken
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public VoteRO addVote(@RequestBody @Valid VoteDto voteDto) {
-        return voteService.createVote(voteDto);
+    public VoteRO addVote(@RequestAttribute Member member,
+                          @RequestBody @Valid VoteDto voteDto) {
+        return voteService.createVote(member, voteDto);
     }
 
     @AuthToken
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteVote(@RequestParam(name = "vote_id") long voteId) {
-        voteService.deleteVote(voteId);
+    public void deleteVote(@RequestAttribute Member member,
+                           @RequestParam(name = "vote_id") long voteId) {
+        voteService.deleteVote(member, voteId);
     }
 
     @GetMapping
     public Page<VoteRO> findVotePage(@RequestParam int page) {
         return voteService.findVotePage(page);
+    }
+
+    @GetMapping("/{vote-id}")
+    public List<TeamResponseDto> findAllTeams(
+            @PathVariable(name = "vote-id") long voteId) {
+        return voteService.findAllTeams(voteId);
     }
 
     @AuthToken
